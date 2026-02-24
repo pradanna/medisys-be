4b8ae1b2a333995b7f41a34cff9c956749dc00bf
bagusyanuar <bagus.yanuar613@gmail.com>
2026-02-24 21:06:24 +0700

    feat: create refresh token using cookie

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
-use App\Http\Requests\Auth\RefreshTokenRequest;
 use App\Http\Resources\Auth\Login\LoginResource;
 use App\Http\Resources\Auth\RefreshToken\RefreshTokenResource;
 use App\Services\Auth\AuthService;
